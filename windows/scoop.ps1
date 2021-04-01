abe4ec24b0632a9699562716b8e4d834305ed69e
elonzh <elonzh@qq.com>
2021-04-01 18:40:20 +0800

    add scoop.ps1

---
+++ windows/scoop.ps1
@@ -0,0 +1,20 @@
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+scoop config proxy 127.0.0.1:7890
+scoop bucket add extras
+scoop install `
+    coreutils `
+    fzf `
+    gettext `
+    git `
+    helm `
+    hugo `
+    kubectl `
+    make `
+    miniconda3 `
+    starship `
+    stern `
+    tldr `
+    vagrant `
+    yarn `
+    z.lua
